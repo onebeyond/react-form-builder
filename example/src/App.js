917bbc324fc3f0cd4a2acb3823aa504e6b211e2b
Ismael Bakkali <ismael.bakkali@guidesmiths.com>
2021-01-15 16:37:52 +0100

    Add support for theming

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
 import { ExampleComponent } from 'react-form-builder'
-import 'react-form-builder/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent text='Create React Library Example 😄' />
 }
 
 export default App
--- example/src/index.js
+++ example/src/index.js
@@ -3,5 +3,12 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { ThemeProvider } from 'theme-ui'
+import { ExampleTheme } from 'react-form-builder'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <ThemeProvider theme={ExampleTheme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root')
+)

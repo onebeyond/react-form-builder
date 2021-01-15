d9610f0399f9d035877d7f5557da16f29edbb3d9
Ismael Bakkali <ismael.bakkali@guidesmiths.com>
2021-01-15 15:56:46 +0100

    Move folder content

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-form-builder'
+import 'react-form-builder/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App

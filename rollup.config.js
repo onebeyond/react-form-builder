b1f00e3ed5581e810b1f654368917c07ed0ff568
Ismael Bakkali <ismael.bakkali@guidesmiths.com>
2021-01-20 11:14:23 +0100

    Support peer dependencies (#7)
    * Add rollup bundler
    
    * Add babel plugin
    
    * Add rollup config file
    
    * Externalize peerDependencies
    
    * Fix output file distribution
    
    * Fix output format
    
    * Add output globals
    
    * Add build with rollup
    
    * Remove microbundle script
    
    * Update pqk lock
    
    * Fix import

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import babel from '@rollup/plugin-babel'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+
+export default {
+  input: 'src/index.js',
+  output: {
+    name: 'ReactFormBuilder',
+    file: 'dist/index.js',
+    format: 'umd',
+    sourcemap: true,
+    globals: { react: 'React', 'theme-ui': 'theme-ui' }
+  },
+  plugins: [peerDepsExternal(), babel()],
+  external: ['react']
+}

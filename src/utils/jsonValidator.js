f2641f8a31689d5148bfc24cf364fd062e4e025c
SofSanUrb <sofiasanchezurbano@gmail.com>
2021-08-02 18:18:50 +0200

    update validation json

--- src/utils/jsonValidator.js
+++ src/utils/jsonValidator.js
@@ -7,7 +7,35 @@ const schema = Joi.object({
   caption: Joi.string().min(3).max(30).required(),
   enabled: Joi.boolean().required(),
   subCaption: Joi.string().min(3),
-  questions: Joi.array().required(),
+  questions: Joi.array()
+    .required()
+    .items(
+      Joi.object({
+        name: Joi.string().required(),
+        type: Joi.string().required(),
+        label: Joi.string().required(),
+        placeholder: Joi.string().required(),
+        icon: Joi.object().required().keys({
+          name: Joi.string(),
+          fill: Joi.string()
+        }),
+        tooltip: Joi.object()
+          .required()
+          .keys({
+            text: Joi.string(),
+            config: Joi.string().keys({
+              backgroundColor: Joi.string()
+            })
+          }),
+        errorMessages: Joi.object().required().keys({
+          required: Joi.string(),
+          pattern: Joi.string()
+        }),
+        registerConfig: Joi.object().required().keys({
+          required: Joi.boolean()
+        })
+      })
+    ),
   question: Joi.array(),
   refrigeratorQuestions: Joi.array(),
   textToShow: Joi.object().required(),

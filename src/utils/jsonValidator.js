2deef96e4b812616d3db51798416dce653858320
SofSanUrb <sofiasanchezurbano@gmail.com>
2021-08-03 18:34:47 +0200

    update json validation dependant questions

--- src/utils/jsonValidator.js
+++ src/utils/jsonValidator.js
@@ -4,7 +4,7 @@ const schema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   title: Joi.string().min(3).max(30).required(),
   layout: Joi.string().min(3).max(30).required(),
-  caption: Joi.string().min(3).max(30).required(),
+  caption: Joi.string().min(3).required(),
   enabled: Joi.boolean().required(),
   subCaption: Joi.string().min(3),
   questions: Joi.array()
@@ -30,22 +30,42 @@ const schema = Joi.object({
             backgroundColor: Joi.string()
           })
         }),
-        errorMessages: Joi.object().keys({
-          required: Joi.string(),
-          pattern: Joi.string(),
-          u18: Joi.string()
-        }),
-        registerConfig: Joi.object().keys({
-          required: Joi.boolean()
-        }),
+        errorMessages: Joi.object(),
+        registerConfig: Joi.object(),
         config: Joi.object().keys({
-          options: Joi.array().items(
-            Joi.object({
-              label: Joi.string(),
-              value: Joi.string()
+          options: Joi.array()
+        }),
+        dependentQuestions: Joi.array().items(
+          Joi.object().keys({
+            condition: Joi.string(),
+            question: Joi.object().keys({
+              name: Joi.string().required(),
+              type: Joi.string().required(),
+              label: Joi.string().required().allow(''),
+              isFullWidth: Joi.boolean(),
+              isBirthDate: Joi.boolean(),
+              openToDate: Joi.string(),
+              minAge: Joi.number(),
+              placeholder: Joi.string().allow(''),
+              priorityOptions: Joi.array(),
+              icon: Joi.object().keys({
+                name: Joi.string(),
+                fill: Joi.string()
+              }),
+              tooltip: Joi.object().keys({
+                text: Joi.string(),
+                config: Joi.object().keys({
+                  backgroundColor: Joi.string()
+                })
+              }),
+              errorMessages: Joi.object(),
+              registerConfig: Joi.object(),
+              config: Joi.object().keys({
+                options: Joi.array()
+              })
             })
-          )
-        })
+          })
+        )
       })
     ),
   question: Joi.array(),

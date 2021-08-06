269a015aa4c17851448b4551d77b50b6a3586b77
Adrian <adrian.rodriguez@guidesmiths.com>
2021-08-06 16:50:59 +0200

    destructuring

--- src/Common/Icon/Icon.js
+++ src/Common/Icon/Icon.js
@@ -4,9 +4,9 @@ import QuestionIcon from './Icons/QuestionIcon'
 import { jsx } from 'theme-ui'
 import style from './icon.module.css'
 
-const Icon = (props) => {
-  const { name, ...iconProperties } = props.icon
-  const { config, textStyle } = props.tooltip
+const Icon = ({ icon, tooltip, ...props }) => {
+  const { name, ...iconProperties } = icon
+  const { config, textStyle, text } = tooltip
 
   return (
     <div className={style.tooltip}>
@@ -14,7 +14,7 @@ const Icon = (props) => {
         'question-circle': <QuestionIcon {...iconProperties} />
       }[name] || <p>Icon Not found</p>}
       <div className={style.top} style={{ ...config }}>
-        <p sx={{ ...textStyle }}>{props.tooltip.text}</p>
+        <p sx={{ ...textStyle }}>{text}</p>
       </div>
     </div>
   )

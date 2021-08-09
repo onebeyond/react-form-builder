1550599f8520d9ef13c045363bfed2967db87126
Adrian <adrian.rodriguez@guidesmiths.com>
2021-08-09 11:02:49 +0200

    Adding the tail at the bottom of the tooltip

--- src/Common/Icon/Icon.js
+++ src/Common/Icon/Icon.js
@@ -3,7 +3,17 @@
 import QuestionIcon from './Icons/QuestionIcon'
 import { jsx } from 'theme-ui'
 import style from './icon.module.css'
-
+const styles = {
+  tooltipArrow: {
+    content: '""',
+    position: 'absolute',
+    width: '12px',
+    height: '12px',
+    left: '50%',
+    transform: 'translate(-50%, -50%) rotate(45deg)',
+    boxShadow: ' 0 1px 8px rgba(0, 0, 0, 0.5)'
+  }
+}
 const Icon = ({ icon, tooltip, ...props }) => {
   const { name, ...iconProperties } = icon
   const { config, textStyle, text } = tooltip
@@ -15,6 +25,14 @@ const Icon = ({ icon, tooltip, ...props }) => {
       }[name] || <p>Icon Not found</p>}
       <div className={style.top} style={{ ...config }}>
         <p sx={{ ...textStyle }}>{text}</p>
+        <i
+          sx={{
+            '::after': {
+              ...styles.tooltipArrow,
+              backgroundColor: config.backgroundColor || 'black'
+            }
+          }}
+        />
       </div>
     </div>
   )

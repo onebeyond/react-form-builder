46e0f1e1d777eb6f0a4ab23f1a8caba4002a1b81
Ivan Esteban <ivan.esteban@guidesmiths.com>
2021-10-15 12:27:54 +0200

    feat: textarea question implemented

---
+++ src/Questions/Textarea/index.js
@@ -0,0 +1,75 @@
+import ErrorMessage from '../../Fields/Error'
+import Textarea from '../../Fields/Textarea'
+import Label from '../../Fields/Label'
+import Icon from '../../Common/Icon/Icon'
+
+/** @jsx jsx */
+/** @jsxRuntime classic */
+import { jsx } from 'theme-ui'
+
+const styles = {
+  boxIconStyle: {
+    display: 'flex'
+  }
+}
+
+const QuestionTextarea = ({ question, useForm }) => {
+  const { register, errors } = useForm
+  const defaultRows = 5
+  console.log('HOLA QUE TAL')
+
+  return (
+    <div
+      sx={{
+        variant: question.id
+          ? 'forms.textareaContainer.' + question.id
+          : 'forms.textareaContainer'
+      }}
+    >
+      <div sx={styles.boxIconStyle}>
+        {question.label && (
+          <Label htmlFor={question.name}>{question.label}</Label>
+        )}
+
+        {question.icon && (
+          <div>
+            <Icon
+              icon={question.icon}
+              tooltip={question.tooltip}
+              sx={{ variant: 'forms.icon' }}
+            />
+          </div>
+        )}
+      </div>
+      <Textarea
+        rows={question.rows || defaultRows}
+        maxLength={question.maxLength}
+        id={question.name}
+        aria-describedby={'error_message_' + question.name}
+        data-testid='question-input'
+        key={question.name}
+        name={question.name}
+        placeholder={question.placeholder}
+        defaultValue={question.defaultValue}
+        ref={register({
+          ...question.registerConfig,
+          pattern: new RegExp(question.registerConfig.pattern)
+        })}
+      />
+      {errors[question.name] && errors[question.name].type === 'required' && (
+        <ErrorMessage
+          name={question.name}
+          message={question.errorMessages && question.errorMessages.required}
+        />
+      )}
+      {errors[question.name] && errors[question.name].type === 'pattern' && (
+        <ErrorMessage
+          name={question.name}
+          message={question.errorMessages && question.errorMessages.pattern}
+        />
+      )}
+    </div>
+  )
+}
+
+export default QuestionTextarea

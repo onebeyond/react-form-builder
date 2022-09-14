048bb737430c4f0a66b59a4d2863bb7a5c62a234
Marisol Peña <65489307+masopego@users.noreply.github.com>
2022-09-14 09:53:36 +0200

    Feat/county field (#118)
    * feat(builder): county field
    
    * feat(tests): county field
    
    * fix(test): county tests
    
    * feat(test): error message and label test
    
    * fix(county): remove ? condition

---
+++ src/Questions/County/_tests_/county.test.js
@@ -0,0 +1,162 @@
+import React from 'react'
+import {
+  cleanup,
+  getByText,
+  render,
+  fireEvent,
+  screen
+} from '@testing-library/react'
+import selectEvent from 'react-select-event'
+import QuestionCountry from '../../Country'
+import QuestionCounty from '../'
+
+afterEach(cleanup)
+
+const question = {
+  type: 'county',
+  label: 'Bundesland',
+  name: 'county',
+  id: '',
+  placeholder: 'Please select an option',
+  country: 'DE',
+  errorMessages: { required: '', pattern: '' },
+  registerConfig: { required: false }
+}
+
+const countryQuestion = {
+  type: 'country',
+  label: 'Country of residence',
+  id: '',
+  name: 'country',
+  placeholder: 'Please make a selection',
+  errorMessages: { required: 'This field is required', pattern: '' },
+  registerConfig: { required: true, pattern: '' },
+  priorityOptions: ['DE', 'GB', 'IE', 'FR'],
+  dependentQuestions: [{ condition: 'DE', label: 'Germany', question }]
+}
+
+const useForm = {
+  errors: {},
+  register: () => {},
+  setValue: jest.fn(),
+  unregister: jest.fn(),
+  trigger: jest.fn()
+}
+
+const setup = () => {
+  const renderComponent = render(
+    <>
+      <QuestionCountry question={countryQuestion} useForm={useForm} />
+      {countryQuestion.dependentQuestions &&
+        countryQuestion.dependentQuestions.map((_, index) => (
+          <QuestionCounty question={question} useForm={useForm} key={index} />
+        ))}
+    </>
+  )
+  const countryComponent = renderComponent.getByTestId('question-country')
+  const countryPlaceholder = renderComponent.getByText(
+    'Please make a selection'
+  )
+  const countyComponent = renderComponent.getByTestId('question-county')
+  const countyPlaceholder = renderComponent.getByText('Please select an option')
+  return {
+    countryComponent,
+    countyComponent,
+    countyPlaceholder,
+    countryPlaceholder
+  }
+}
+
+test('check the placeholder text', () => {
+  const { countyComponent } = setup()
+  getByText(countyComponent, 'Please select an option')
+})
+
+test('County label', () => {
+  const { countyComponent } = setup()
+  getByText(countyComponent, 'Bundesland')
+})
+
+test('label tag is not displayed when label value is null', () => {
+  const questionNoLabel = { ...question }
+  delete questionNoLabel.label
+  render(<QuestionCounty question={questionNoLabel} useForm={useForm} />)
+
+  expect(!screen.queryByTestId('county-label'))
+})
+
+test('County select appears when country question is clicked and GE value', async () => {
+  const { countryPlaceholder } = setup()
+
+  await selectEvent.openMenu(countryPlaceholder)
+  fireEvent.keyDown(countryPlaceholder, { key: 'Enter', code: 13 })
+  await expect(screen.getByText('Germany'))
+  expect(screen.getByText('Please select an option'))
+})
+
+test('County select is opened on the right country region', async () => {
+  const { countryPlaceholder, countyPlaceholder } = setup()
+
+  await selectEvent.openMenu(countryPlaceholder)
+  fireEvent.keyDown(countryPlaceholder, { key: 'Enter', code: 13 })
+  await expect(screen.getByText('Germany'))
+  expect(screen.getByText('Please select an option'))
+
+  await selectEvent.openMenu(countyPlaceholder)
+  fireEvent.keyDown(countyPlaceholder, { key: 'Enter', code: 13 })
+  await expect(screen.getByText('Baden-Württemberg'))
+})
+
+test('renders a fallback country list when country condition is not supported', async () => {
+  const question = {
+    type: 'county',
+    label: 'County',
+    name: 'county',
+    id: '',
+    placeholder: 'Please select an option',
+    country: '',
+    errorMessages: { required: '', pattern: '' },
+    registerConfig: { required: false }
+  }
+
+  const { getByText } = render(
+    <QuestionCounty question={question} useForm={useForm} />
+  )
+
+  const countySelect = getByText('Please select an option')
+
+  await selectEvent.openMenu(countySelect)
+  fireEvent.keyDown(countySelect, { key: 'Enter', code: 13 })
+  expect(screen.getByText('Aberdeen City'))
+})
+
+test('show an error message', () => {
+  const question = {
+    type: 'county',
+    label: 'County',
+    name: 'county',
+    id: '',
+    placeholder: 'Please select an option',
+    country: '',
+    errorMessages: { required: 'This question is required', pattern: '' },
+    registerConfig: { required: true }
+  }
+
+  const { getByText } = render(
+    <QuestionCounty
+      question={question}
+      useForm={{
+        errors: {
+          [question.name]: {
+            type: 'required'
+          }
+        },
+        register: () => {},
+        setValue: jest.fn(),
+        unregister: jest.fn(),
+        trigger: jest.fn()
+      }}
+    />
+  )
+  expect(getByText(question.errorMessages.required)).toBeTruthy()
+})
---
+++ src/Questions/County/index.js
@@ -0,0 +1,77 @@
+/** @jsx jsx */
+/** @jsxRuntime classic */
+import { jsx } from 'theme-ui'
+import countryAndRegions from './data/countryAndCounties'
+import Select from '../../Fields/Select'
+import Label from '../../Fields/Label'
+import ErrorMessage from '../../Fields/Error'
+
+const QuestionCounty = ({ question, useForm, ...props }) => {
+  const { errors, register, setValue, unregister, trigger } = useForm
+
+  const getRegionOptions = (country) => {
+    const list =
+      countryAndRegions.find((item) => item.cs === country) &&
+      countryAndRegions
+        .find((item) => item.cs === country)
+        .regions.map((option) => ({
+          label: option.cn,
+          value: option.cn.toLowerCase().replace(/ /g, '_')
+        }))
+
+    return list
+  }
+
+  const renderCountyOptions = (items) =>
+    items.map((item) => (
+      <option key={item.value} value={item.value}>
+        {item.label}
+      </option>
+    ))
+  const countrySelected = question.country || 'GB'
+  const options = getRegionOptions(countrySelected)
+
+  return (
+    <div
+      data-testid='question-county'
+      sx={{
+        variant: question.id
+          ? 'forms.countyContainer.' + question.id
+          : 'forms.countyContainer'
+      }}
+    >
+      {question.label && (
+        <Label htmlFor={question.name} data-testid='county-label'>
+          {question.label}
+        </Label>
+      )}
+      <Select
+        onChange={() => trigger(question.name)}
+        id={question.name}
+        key={question.name}
+        name={question.name}
+        options={options}
+        isSearchable={false}
+        register={register}
+        registerConfig={question.registerConfig}
+        setValue={setValue}
+        placeholder={question.placeholder}
+        unregister={unregister}
+        label={question.label}
+        {...props}
+      >
+        {renderCountyOptions(options)}
+      </Select>
+      {errors[question.name] &&
+        (errors[question.name].type === 'required' ||
+          errors[question.name].type === 'noEmpty') && (
+          <ErrorMessage
+            name={question.name}
+            message={question.errorMessages && question.errorMessages.required}
+          />
+        )}
+    </div>
+  )
+}
+
+export default QuestionCounty

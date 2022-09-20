917b20930f7abdd5b5cd9674944c8c5417c08568
Marisol Pena <masopego@gmail.com>
2022-09-20 15:33:09 +0200

    feat(questions): add gender options by locale

---
+++ src/Questions/Genre/data/es.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    label: 'Masculino',
+    value: 'male'
+  },
+  {
+    label: 'Femenino',
+    value: 'female'
+  },
+  {
+    label: 'Otro',
+    value: 'other'
+  },
+  {
+    label: 'Prefiero no decirlo',
+    value: 'prefer_not_to_say'
+  }
+]
---
+++ src/Questions/Genre/data/fr.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    label: 'Masculin',
+    value: 'male'
+  },
+  {
+    label: 'Féminin',
+    value: 'female'
+  },
+  {
+    label: 'Autre',
+    value: 'other'
+  },
+  {
+    label: 'Préfère ne pas préciser',
+    value: 'prefer_not_to_say'
+  }
+]

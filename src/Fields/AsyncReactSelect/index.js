9deadf20cc18208c2d0bb253be4c4ec579c70a7f
Pablo Eliseo <pabloelisseo@gmail.com>
2023-05-25 11:06:03 +0200

    fix(react-select): use id to prevent hydration errors

--- src/Fields/AsyncReactSelect/index.js
+++ src/Fields/AsyncReactSelect/index.js
@@ -1,6 +1,6 @@
 /** @jsxRuntime classic */
 
-import React from 'react'
+import React, { useId } from 'react'
 
 import { useThemeUI } from 'theme-ui'
 import { RHFInput } from 'react-hook-form-input'
@@ -90,7 +90,14 @@ const Select = ({
   return (
     <RHFInput
       onChange={onChange}
-      as={<AsyncSelect aria-label={label} styles={customStyles} {...props} />}
+      as={
+        <AsyncSelect
+          aria-label={label}
+          styles={customStyles}
+          id={useId()}
+          {...props}
+        />
+      }
       rules={{
         ...registerConfig,
         validate: {

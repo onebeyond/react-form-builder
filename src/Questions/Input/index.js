92dcff2477556c68313806a8964c4019fd6cebd5
NuriaExtremadouro <nuria.extremadouro@gmail.com>
2023-06-23 09:44:33 +0200

    Use numbers as booleans to fix warning

--- src/Questions/Input/index.js
+++ src/Questions/Input/index.js
@@ -51,7 +51,7 @@ const QuestionInput = ({ question, useForm, component }) => {
         type={question.type}
         placeholder={question.placeholder}
         defaultValue={question.defaultValue}
-        haserrors={!!errors[question.name]}
+        haserrors={errors[question.name] ? 1 : 0}
         {...register(question.name, {
           ...question.registerConfig,
           pattern: new RegExp(question.registerConfig.pattern)
